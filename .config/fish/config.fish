b91ced7a33335e88e5c02a27bf6a6d0400fe3dcc
Yiyao Wei <yiyao.wei@icloud.com>
2021-06-26 12:29:50 +0200

    🔧 Update fish config and setup

---
+++ .config/fish/config.fish
@@ -0,0 +1,46 @@
+set -gx PATH /usr/local/bin $PATH
+
+
+if type -q exa
+  alias ll "exa -l -g --icons"
+  alias lla "ll -a"
+ end
+
+
+
+
+# Aliases
+alias dotfiles "/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+alias fishconf "vim ~.config/fish/config.fish"
+alias vimrc "vim ~/.vimrc"
+alias tmux.conf "vim ~/.tmux.conf"
+alias ohmyzsh "vim ~/.oh-my-zsh"
+alias sshconfig "vim ~/.ssh/config"
+alias pip "pip3"
+alias python "python3"
+alias update_comet_ml "pip3 install comet_ml --upgrade --upgrade-strategy eager"
+alias code "code-insiders"
+ 
+
+# BasicTeX
+alias texupdate "sudo tlmgr update --self && sudo tlmgr update --all"
+alias texinstall "sudo tlmgr install"
+set -gx PATH "/usr/local/sbin:$PATH"
+set -gx MANPATH "/Library/TeX/Distributions/.DefaultTeX/Contents/Man:$MANPATH"
+
+# qt
+set -gx PATH "/usr/local/opt/qt/bin:$PATH"
+set -gx LDFLAGS "-L/usr/local/opt/qt/lib"
+set -gx CPPFLAGS "-I/usr/local/opt/qt/include"
+set -gx PKG_CONFIG_PATH "/usr/local/opt/qt/lib/pkgconfig"
+
+# ruby
+set -gx PATH "/usr/local/opt/ruby/bin:$PATH"
+set -gx LDFLAGS "/usr/local/opt/ruby/lib"
+set -gx CPPFLAGS "/usr/local/opt/ruby/include"
+set -gx PKG_CONFIG_PATH "/usr/local/opt/ruby/lib/pkgconfig"
+set -gx PATH "$HOME/.gem/ruby/2.7.2/bin:$PATH"
+
+# Poetry
+set -gx PATH "$HOME/.poetry/bin:$PATH"
+
--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ xcode-select --install
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 # CLI tools
-brew install git zsh tmux svn tldr yank parallel httpie
+brew install git zsh tmux svn tldr yank parallel httpie exa fzf
 
 # QuickLook tools on MacOS
 brew install qlcolorcode qlstephen qlmarkdown quicklook-json qlimagesize suspicious-package apparency quicklookase qlvideo
@@ -21,18 +21,13 @@ brew intall --cask notion typora raycast iterm2 visual-studio-code zoom
 brew install kubectl helm kube-ps1 kubectx stern
 brew install --cask google-cloud-sdk docker
 
-# oh-my-zsh & plugins
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-# zsh-completions
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
-# zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-# zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# fish & fisher
+brew install fish 
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 
 # Fonts
 brew tap homebrew/cask-fonts
-brew install font-inconsolata font-fira-code font-inconsolata-g-for-powerline
+brew install font-inconsolata font-fira-code font-inconsolata-g-for-powerline font-inconsolata-lgc-nerd-font
 
 # Logitech options for my mouse configuration
 brew install homebrew/cask-drivers/logitech-options
