4cc826ffcdcb5aad8da1b457587e4d9c75c26c9b
Yiyao Wei <yiyao.wei@icloud.com>
2021-07-10 13:51:58 +0200

    ✨ More alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,10 +4,14 @@ fish_vi_key_bindings
 if type -q exa
   alias ll "exa -l -g --icons"
   alias lla "ll -a"
+  alias ls "exa --tree --level=1 --icons"
 end
 
 
-# Aliases
+
+## Alias & Env
+
+# Misc.
 alias dotfiles "/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
 alias fishconf "vim ~/.config/fish/config.fish"
 alias vimrc "vim ~/.vimrc"
@@ -18,7 +22,17 @@ alias pip "pip3"
 alias python "python3"
 alias update_comet_ml "pip3 install comet_ml --upgrade --upgrade-strategy eager"
 alias code "code-insiders"
- 
+
+# Brew
+alias brewp "brew pin"
+alias brews "brew list -1"
+alias brewsp "brew list --pinned"
+alias bubo "brew update && brew outdated"
+alias bubc "brew upgrade && brew cleanup"
+alias bubu "bubo && bubc"
+alias buf "brew upgrade --formula"
+alias bubuc "brew outdated --cask & brew upgrade --cask && brew cleanup"
+
 
 # BasicTeX
 alias texupdate "sudo tlmgr update --self && sudo tlmgr update --all"
@@ -41,4 +55,7 @@ set PATH $HOME/.gem/ruby/2.7.2/bin $PATH
 
 # Poetry
 set PATH $HOME/.poetry/bin $PATH
+
+
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
