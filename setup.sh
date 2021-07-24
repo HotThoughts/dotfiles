55aca11ba5299bba35db5e5670d31c96799d51a1
Yiyao Wei <yiyao.wei@icloud.com>
2021-07-24 22:58:00 +0200

    🔧 Refactor

--- setup.sh
+++ setup.sh
@@ -9,20 +9,20 @@ xcode-select --install
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 # CLI tools
-brew install git zsh tmux svn isacikgoz/taps/tldr yank parallel httpie exa fzf git-delta
+brew install git tmux svn isacikgoz/taps/tldr yank parallel httpie exa fzf git-delta
 
 # QuickLook tools on MacOS
 brew install qlcolorcode qlstephen qlmarkdown quicklook-json qlimagesize suspicious-package apparency quicklookase qlvideo
 
 # Must-have softwares
-brew intall --cask notion typora raycast iterm2 visual-studio-code zoom
+brew intall --cask notion typora raycast alacritty visual-studio-code zoom
 
 # DevOps tools
 brew install kubectl helm kube-ps1 kubectx stern
 brew install --cask google-cloud-sdk docker
 
 # fish & fisher
-brew install fish 
+brew install fish
 curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 
 # Fonts
