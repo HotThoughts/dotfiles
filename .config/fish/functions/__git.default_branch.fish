1c85681b583b43564178ddb0ba4a1722c76d3222
Yiyao Wei <yiyao.wei@icloud.com>
2021-08-05 11:48:34 +0200

    🔧 Remove abbr tips and refactor functions

--- .config/fish/functions/__git.default_branch.fish
+++ .config/fish/functions/__git.default_branch.fish
@@ -1,10 +1,11 @@
 function __git.default_branch -d "Fallback to main when master branch is not found"
-  # Silently fail when not in git directory
   command git rev-parse --git-dir &>/dev/null; or return
-  if command git show-ref -q --verify refs/heads/master
+  if set -l default_branch (command git config --get init.defaultBranch)
+    and command git show-ref -q --verify refs/heads/{$default_branch}
+    echo $default_branch
+  else if command git show-ref -q --verify refs/heads/master
     echo master
   else
     echo main
   end
-end  
-
+end
--- .config/fish/functions/exa.ll.fish
+++
@@ -1,5 +0,0 @@
-if type -q exa
-  function ll
-    exa -l -g --icons $argv
-  end
-end
