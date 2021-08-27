ec386c5b7440f5f28f10862a739636fa539d4818
Yiyao Wei <yiyao.wei@icloud.com>
2021-08-27 09:49:11 +0200

    🔧 Add neoscroll

--- .config/lvim/lv-config.lua
+++ .config/lvim/lv-config.lua
@@ -8,7 +8,7 @@ vim.opt.relativenumber = true
 
 -- Color Scheme
 lvim.colorscheme = "tokyonight"
-lvim.transparent_window = true
+lvim.transparent_window = false
 
 -- keymappings
 lvim.leader = "space"
@@ -124,7 +124,26 @@ lvim.plugins = {
   {
     "folke/trouble.nvim",
     cmd = "TroubleToggle",
-  }
+  },
+  {
+    "karb94/neoscroll.nvim",
+    event = "WinScrolled",
+    config = function()
+    require('neoscroll').setup({
+          -- All these keys will be mapped to their corresponding default scrolling animation
+          mappings = {'<C-u>', '<C-d>', '<C-b>', '<C-f>',
+          '<C-y>', '<C-e>', 'zt', 'zz', 'zb'},
+          hide_cursor = true,          -- Hide cursor while scrolling
+          stop_eof = true,             -- Stop at <EOF> when scrolling downwards
+          use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
+          respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+          cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+          easing_function = nil,        -- Default easing function
+          pre_hook = nil,              -- Function to run before the scrolling animation starts
+          post_hook = nil,              -- Function to run after the scrolling animation ends
+          })
+    end
+  },
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
@@ -153,6 +172,6 @@ lvim.builtin.which_key.mappings["t"] = {
   r = { "<cmd>TroubleToggle lsp_references<cr>", "References" },
   c = { "<cmd>TodoTrouble<cr>", "Todo Comments" },
 }
-lvim.builtin.which_key.mappings['s'] = {
-  t = { "<cmd>TodoTelescope<cr>", "Todo Comments" }
-}
+-- lvim.builtin.which_key.mappings['s'] = {
+--   t = { "<cmd>TodoTelescope<cr>", "Todo Comments" }
+-- }
