f241ccbc8ed7a19a7b94322f1ae5ec56540e16f3
Yiyao Wei <yiyao.wei@icloud.com>
2021-09-09 23:18:42 +0200

    💫 Sync config with remote

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -3,8 +3,8 @@
 -- general
 lvim.format_on_save = true
 lvim.lint_on_save = true
-lvim.builtin.nvimtree.hide_dotfiles = 0
 vim.opt.relativenumber = true
+vim.opt.wrap = true
 
 -- Color Scheme
 lvim.colorscheme = "tokyonight"
@@ -12,49 +12,41 @@ lvim.transparent_window = false
 
 -- keymappings
 lvim.leader = "space"
--- overwrite the key-mappings provided by LunarVim for any mode, or leave it empty to keep them
--- lvim.keys.normal_mode = {
--- --   Page down/up
--- --   {'[d', '<PageUp>'},
--- --   {']d', '<PageDown>'},
--- --
--- --   Navigate buffers
---   {'<Tab>', ':bnext<CR>'},
---   {'<S-Tab>', ':bprevious<CR>'},
---   {'Y', 'y$'},
---   {'n', 'nzzzv'},
---   {'N', 'Nzzzv'},
---   {'J', 'mzJ`z'},
--- }
--- lvim.keys.insert_mode = {
---   {',', ',<c-g>u'},
---   {'.', '.<c-g>u'},
---   {'!', '!<c-g>u'},
---   {'?', '?<c-g>u'},
--- }
--- lvim.keys.visual_mode = {
---   -- move lines visually
--- }
--- if you just want to augment the existing ones then use the utility function
--- require("utils").add_keymap_insert_mode({ silent = true }, {
--- { "<C-s>", ":w<cr>" },
--- { "<C-c>", "<ESC>" },
--- })
--- you can also use the native vim way directly
--- vim.api.nvim_set_keymap("i", "<C-Space>", "compe#complete()", { noremap = true, silent = true, expr = true })
+-- add your own keymapping
+-- lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+
+-- unmap a default keymapping
+-- lvim.keys.normal_mode["<C-Up>"] = ""
+-- edit a default keymapping
+-- lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
+
+-- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
+-- lvim.builtin.telescope.on_config_done = function()
+--   local actions = require "telescope.actions"
+--   -- for input mode
+--   lvim.builtin.telescope.defaults.mappings.i["<C-j>"] = actions.move_selection_next
+--   lvim.builtin.telescope.defaults.mappings.i["<C-k>"] = actions.move_selection_previous
+--   lvim.builtin.telescope.defaults.mappings.i["<C-n>"] = actions.cycle_history_next
+--   lvim.builtin.telescope.defaults.mappings.i["<C-p>"] = actions.cycle_history_prev
+--   -- for normal mode
+--   lvim.builtin.telescope.defaults.mappings.n["<C-j>"] = actions.move_selection_next
+--   lvim.builtin.telescope.defaults.mappings.n["<C-k>"] = actions.move_selection_previous
+-- end
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.dashboard.active = true
 lvim.builtin.terminal.active = true
+
 lvim.builtin.nvimtree.side = "left"
 lvim.builtin.nvimtree.show_icons.git = 1
+lvim.builtin.nvimtree.hide_dotfiles = 0
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {}
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
-
+lvim.builtin.treesitter.matchup = true
 -- generic LSP settings
 -- you can set a custom on_attach function that will be used for all the language servers
 -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
