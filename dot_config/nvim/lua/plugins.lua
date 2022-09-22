4fe897001a930549f4bf1201dc1c708e12a2ce81
Yiyao Wei <26658608+HotThoughts@users.noreply.github.com>
2022-09-22 02:17:29 +0200

    ♻️ refactor nvim config for vscode

---
+++ dot_config/nvim/lua/plugins.lua
@@ -0,0 +1,8 @@
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  -- Quicker edit
+  use 'tpope/vim-surround'
+  -- Fast Movement
+  use 'unblevable/quick-scope'
+  use 'ggandor/lightspeed.nvim'
+end)
