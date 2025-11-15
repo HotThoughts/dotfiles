2b671641031661699d6a7f0424ad930bec00420b
Yiyao Wei <26658608+HotThoughts@users.noreply.github.com>
2025-11-15 14:31:49 +0100

    🔧 Disable some markdownline rules

--- dot_config/nvim/lua/plugins/lang.lua
+++ dot_config/nvim/lua/plugins/lang.lua
@@ -1,4 +1,5 @@
 return {
+  -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
   {
     "mason-org/mason.nvim",
     opts = {
@@ -14,6 +15,45 @@ return {
         "hadolint",
         "helm-ls",
         "json-lsp",
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        ["yaml"] = { "yamlfix" },
+      },
+      formatters = {
+        yamlfix = {
+          env = {
+            YAMLFIX_preserve_quotes = true,
+            YAMLFIX_WHITELINES = "1",
+            YAMLFIX_EXPLICIT_START = false,
+            YAMLFIX_SEQUENCE_STYLE = "block_style",
+          },
+        },
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        markdown = {
+          "markdownlint",
+        },
+      },
+      linters = {
+        markdownlint = {
+          args = {
+            "--disable",
+            "MD041",
+            "--disable",
+            "MD013",
+          },
+        },
       },
     },
   },
