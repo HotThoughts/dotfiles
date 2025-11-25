eab41eb76bc691f166b03ada02d2f11488880c85
Yiyao Wei <26658608+HotThoughts@users.noreply.github.com>
2025-11-25 10:50:13 +0100

    💄 Increase nvim line space

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Line spacing (spacing between lines)
+-- Increase this value for more space between lines
+vim.opt.linespace = 2 -- Adjust this value (0 = default, higher = more space)
