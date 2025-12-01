a9e222a319103ce2961611823570762b617802be
Yiyao Wei <26658608+HotThoughts@users.noreply.github.com>
2025-12-02 00:50:32 +0100

    🧽 Remove auto-save

--- dot_config/nvim/lua/plugins/essentials.lua
+++ dot_config/nvim/lua/plugins/essentials.lua
@@ -113,14 +113,4 @@ return {
       end,
     },
   },
-  {
-    "Pocco81/auto-save.nvim",
-    opts = {
-      execution_message = {
-        message = function() -- message to print on save
-          return ""
-        end,
-      },
-    },
-  },
 }
