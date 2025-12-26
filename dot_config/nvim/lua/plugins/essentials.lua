88dc20a7ecc6fdffd81948f7af4bedce553360aa
Yiyao Wei <26658608+HotThoughts@users.noreply.github.com>
2025-12-26 21:45:39 +0100

    🔧 Update nvim config and sketchybar workspace icons

--- dot_config/nvim/lua/plugins/essentials.lua
+++ dot_config/nvim/lua/plugins/essentials.lua
@@ -87,30 +87,27 @@ return {
 
       -- see below for full list of options 👇
     },
-    {
-      "HotThoughts/jjui.nvim",
-      dependencies = {
-        "nvim-lua/plenary.nvim", -- required
-      },
-      cmd = {
-        "JJUI",
-        "JJUICurrentFile",
-        "JJUIFilter",
-        "JJUIFilterCurrentFile",
-        "JJConfig",
-      },
-      -- Setting the keybinding here helps lazy-loading
-      keys = {
-        { "<leader>jj", "<cmd>JJUI<cr>", desc = "JJUI" },
-        { "<leader>jc", "<cmd>JJUICurrentFile<cr>", desc = "JJUI (current file)" },
-        { "<leader>jl", "<cmd>JJUIFilter<cr>", desc = "JJUI Log" },
-        { "<leader>jf", "<cmd>JJUIFilterCurrentFile<cr>", desc = "JJUI Log (current file)" },
-      },
-      config = function()
-        require("jjui").setup({
-          -- configuration options (see below)
-        })
-      end,
+  },
+  {
+    "HotThoughts/jjui.nvim",
+    cmd = {
+      "JJUI",
+      "JJUICurrentFile",
+      "JJUIFilter",
+      "JJUIFilterCurrentFile",
+      "JJConfig",
     },
+    -- Setting the keybinding here helps lazy-loading
+    keys = {
+      { "<leader>jj", "<cmd>JJUI<cr>", desc = "JJUI" },
+      { "<leader>jc", "<cmd>JJUICurrentFile<cr>", desc = "JJUI (current file)" },
+      { "<leader>jl", "<cmd>JJUIFilter<cr>", desc = "JJUI Log" },
+      { "<leader>jf", "<cmd>JJUIFilterCurrentFile<cr>", desc = "JJUI Log (current file)" },
+    },
+    config = function()
+      require("jjui").setup({
+        -- configuration options (see below)
+      })
+    end,
   },
 }
